d68238ad10e19baf80013592ffb013ec0eb0363f
user <esy_win@hotmail.com>
2022-03-14 21:41:17 +0100

    added missing shebang and curl piped into bash

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # komodo-install
+
+```bash
+curl --proto=https -sSf https://github.com/EsyWin/komodo-install/blob/main/install.sh | bash -
+```
--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
-#The following packages are needed:
+#!/bin/bash
+# Installing dependencies
 sudo apt-get install build-essential pkg-config libc6-dev m4 g++-multilib autoconf libtool ncurses-dev unzip git python python-zmq zlib1g-dev wget libcurl4-gnutls-dev bsdmainutils automake curl libsodium-dev
-
+# navigate to home folder to ensure consistent installs
+cd ~
+# clone komodo git repo
 git clone https://github.com/komodoplatform/komodo --branch master --single-branch
+# navigate into komodo folder
 cd komodo
+# fetch komodo-zcash parameters
 ./zcutil/fetch-params.sh
+# build (this takes long time)
 ./zcutil/build.sh -j$(expr $(nproc) - 1)
-#This can take some time.
